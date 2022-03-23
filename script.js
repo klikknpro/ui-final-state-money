d47c8a568a6c7e7ff6302c4611b4cadd54f77a8b
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-03-23 16:38:15 +0100

    init setup and data

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
+const currencies = [
+  {
+    title: "US Dollar",
+    sub: "$",
+    text: "USD",
+  },
+  {
+    title: "Euro",
+    sub: "€",
+    text: "EUR",
+  },
+  {
+    title: "Chinese Yuan",
+    sub: "¥",
+    text: "CNY",
+  },
+  {
+    title: "Thai Baht",
+    sub: "฿",
+    text: "THB",
+  },
+  {
+    title: "British Pound Sterling",
+    sub: "£",
+    text: "GBP",
+  },
+  {
+    title: "Ghanaian Cedi",
+    sub: "₵",
+    text: "GHS",
+  },
+  {
+    title: "Japanese Yen",
+    sub: "￥",
+    text: "JPY",
+  },
+  {
+    title: "Polish Zloty",
+    sub: "zł",
+    text: "PLN",
+  },
+];
+
 function init() {}
 
 window.addEventListener("load", init);
-
-/*
-window.addEventListener("load", function(){
-});
-window.addEventListener("load", () => {
-});
-*/
